9485afc21094cec7785511b790fec51461ce38a4
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-10-08 16:39:38 +0200

    Initial commit

---
+++ components/Contact.js
@@ -0,0 +1,14 @@
+class Contact extends React.Component {
+    propTypes = {
+        item: PropTypes.object.isRequired,
+    }
+    render() {
+        return 
+            <div className={"contactItem"}>
+                <img className={"contactImage"} src={"http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico"}/>
+                <p className={"contactLabel"}>Imię: {{this.props.item.firstName}}</p>
+                <p className={"contactLabel"}>Nazwisko: {{this.props.item.lastName}}</p>
+                <a className={"contactEmail"} href={"mailto: " this.props.item.email}>{{this.props.item.email}}</a>
+            </div>
+    }
+}
---
+++ components/Contacts.js
@@ -0,0 +1,12 @@
+class Contacts extends React.Component {
+   propTypes = {
+        items: PropTypes.array.isRequired,
+    }
+    render () {
+        var contacts = this.props.items.map(function(contact) {
+            return <Contact item={"contact"} key={"contact.id"} />
+        });
+
+        return <ul className={"contactsList"}>contacts</ul>
+    }
+};
